d3b9a5ad979081578df1fee8592ed1664c67431a
antiennn <antien2k3@gmail.com>
2024-08-31 23:52:00 +0700

    chore: add icon weather

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import Sunny from "./image/sunny.png";
+import Cloud from "./image/clouds.png";
+import Rain from "./image/rainy.png";
+import CloudDay from "./image/cloudy-day.png";
+
+export const images = {
+  sunny: Sunny,
+  Cloud: Cloud,
+  Cloudly: CloudDay,
+  Rainly: Rain,
+};
