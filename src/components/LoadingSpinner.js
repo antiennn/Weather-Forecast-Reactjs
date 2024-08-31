71ec985e94ab8665ceef149b1fa250dd774e91c2
antiennn <antien2k3@gmail.com>
2024-08-31 23:55:31 +0700

    feat!: add loading animation when get data

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingSpinner = () => (
+    <div className="border-t-4 border-b-4 border-indigo-600 w-16 h-16 border-solid rounded-full animate-spin"></div>
+);
+
+export default LoadingSpinner;
